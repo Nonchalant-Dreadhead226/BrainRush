612b2024fba999b7427cd18d2c8605fdcaa95278
Nonchalant-Dreadhead226 <gustavs.cacs0709@gmail.com>
2026-06-05 23:45:46 +0300

    functions.php

--- functions.php
+++ functions.php
@@ -3,12 +3,11 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $has_register_flag = !empty($_SESSION['signup_error']) || !empty($_SESSION['signup_success']);
-    $has_login_flag    = !empty($_SESSION['login_failed']);
+    $has_login_flag = !empty($_SESSION['login_failed']);
 
     if (!$has_register_flag) {
         $_SESSION['AddRegisterDisplay'] = 'style="display:none"';
     }
-
     if (!$has_login_flag) {
         $_SESSION['AddLoginDisplay'] = 'style="display:none"';
     }
@@ -17,15 +16,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['register'])) {
         $_SESSION['AddRegisterDisplay'] = 'style="display:block"';
-        $_SESSION['AddLoginDisplay']    = 'style="display:none"';
+        $_SESSION['AddLoginDisplay'] = 'style="display:none"';
     }
     if (isset($_POST['login'])) {
-        $_SESSION['AddLoginDisplay']    = 'style="display:block"';
+        $_SESSION['AddLoginDisplay'] = 'style="display:block"';
         $_SESSION['AddRegisterDisplay'] = 'style="display:none"';
     }
     if (isset($_POST['atcelt'])) {
         $_SESSION['AddRegisterDisplay'] = 'style="display:none"';
-        $_SESSION['AddLoginDisplay']    = 'style="display:none"';
+        $_SESSION['AddLoginDisplay'] = 'style="display:none"';
     }
     if (isset($_POST['dashboard'])) {
         header('Location: index.php');
